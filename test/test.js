a968fd0549b01cdedc6e994278b89db6295eb9c9
Leon Chen <lchen3@gmail.com>
2015-05-04 16:05:29 -0400

    update package.json to reflect name change to doco-ontology

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var doco = require('..');
 var assert = require('assert');
 
-describe('schema-components', function() {
+describe('doco-ontology', function() {
   it('should be available as a JS object', function() {
     assert(doco.defines);
   });
