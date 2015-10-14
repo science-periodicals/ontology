7ad113a0a6543ef7db740c8ec4e26e6f61af251f
Robin Berjon <robin@berjon.com>
2015-10-14 10:58:45 -0400

    add foonotes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, 'article.jsonld')), 'utf8');
+// module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, 'article.jsonld')), 'utf8');
+module.exports = require('./article.json');
