5dfc21b1b6a7120fc5d57936c863ebcb8e81c6da
sballesteros <sebastien.ballesteros@gmail.com>
2016-10-19 15:03:17 -0400

    add more prefixes

--- prefixes.js
+++ prefixes.js
@@ -1,4 +1,9 @@
 module.exports = {
+  xml: 'http://www.w3.org/XML/1998/namespace',
+  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+  rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+  owl: 'http://www.w3.org/2002/07/owl#',
+  schema: 'http://schema.org/',
   xsd: "http://www.w3.org/2001/XMLSchema#", // make sure that xsd prefix is present as we use it for typed dates
   sa: "http://ns.science.ai#",
   user: "https://science.ai/user/",
